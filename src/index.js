15d06f32c398d46f672ac6a3c47097068f31c163
Nylbert Victor <nyldrift@hotmail.com>
2023-05-07 15:13:01 -0300

    CRUD functions added
    New functionalities added:
    - Add new itens
    - remove itens clicking on the options button and on the thrash
    - marking the checkbox will mark the item as completed
    - editing the item and clicking ouside erase the change
    - editing the item and clicking on the confirm button saves the item
    - clicking on Clear all completed will remove the taks marked as completed
    - all itens now are saved and retrieved from local storage on the browser

--- src/index.js
+++ src/index.js
@@ -1,74 +1,34 @@
 import './index.css';
-
-const list = [
-  {
-    description: 'First Item',
-    status: false,
-    index: 0,
-  },
-  {
-    description: 'Second Item',
-    status: false,
-    index: 1,
-  },
-  {
-    description: 'Third Item',
-    status: false,
-    index: 2,
-  },
-];
-
-function buildTask(i) {
-  const taskList = document.getElementById('list-task');
-  const taskItem = document.createElement('li');
-  taskItem.classList.add('task');
-  taskItem.id = list[i].index;
-  const taskContDiv = document.createElement('div');
-  taskContDiv.classList.add('align-middle', 'task-content');
-  taskItem.append(taskContDiv);
-
-  const itemCheckBox = document.createElement('input');
-  itemCheckBox.type = 'checkbox';
-  itemCheckBox.name = list[i].index;
-  taskContDiv.append(itemCheckBox);
-
-  const taskDescription = document.createElement('input');
-  taskDescription.classList.add('task-description');
-  taskDescription.value = list[i].description;
-  taskDescription.type = 'text';
-  taskContDiv.append(taskDescription);
-
-  const itemOptions = document.createElement('i');
-  itemOptions.classList.add('fa-solid', 'fa-ellipsis-vertical');
-  itemOptions.setAttribute = ('onclick', 'deleteHid()');
-  taskItem.append(itemOptions);
-
-  const itemRemove = document.createElement('i');
-  itemRemove.classList.add('fa-solid', 'fa-trash', 'hidden');
-  itemRemove.style.color = '#fff';
-  itemRemove.setAttribute = ('onclick', 'deleteHid()');
-  taskItem.append(itemRemove);
-
-  taskList.append(taskItem);
-}
+import { buildTask, list, saveData } from './modules/crud.js';
 
 const addTask = document.getElementById('form');
+let data = [];
 
 window.addEventListener('load', () => {
-  for (let i = 0; i < list.length; i += 1) {
-    buildTask(i);
-  }
+    const savedData = JSON.parse(localStorage.getItem('taskList'));
+    if(savedData !== null)
+    {
+        for (let i = 0; i < savedData.length; i += 1) {
+            data = [{
+                'description': savedData[i].description,
+                'status': savedData[i].status,
+                'index': savedData[i].index,
+            }];
+            buildTask(data,i);
+        }
+    }
 });
 
 addTask.addEventListener('submit', (event) => {
   event.preventDefault();
-  list.push(
-    {
-      description: addTask.newtask.value,
-      status: false,
-      index: list.length,
-    },
-  );
+  data = [{
+      'description': addTask.newtask.value,
+      'status': false,
+      'index': list.length,
+    }];
   addTask.newtask.value = '';
-  buildTask(list.length - 1);
+  buildTask(data,0);
+  saveData(list);
 });
+
+export default data;
